1352cab8c8c8178da2e90e466bbca5dfd6390397
Romeo Balta <7095569+romeobalta@users.noreply.github.com>
2024-03-05 23:56:17 +0000

    revert

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -113,18 +113,4 @@ return {
       keys[#keys + 1] = newKey
     end
   end,
-  opts = {
-    servers = { eslint = {} },
-    setup = {
-      eslint = function()
-        require("lazyvim.util").lsp.on_attach(function(client)
-          if client.name == "eslint" then
-            client.server_capabilities.documentFormattingProvider = true
-          elseif client.name == "tsserver" then
-            client.server_capabilities.documentFormattingProvider = false
-          end
-        end)
-      end,
-    },
-  },
 }
