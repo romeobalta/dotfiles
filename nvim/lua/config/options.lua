c23504c8b7c755711026cf699d51da6cf05fb175
Romeo Balta <7095569+romeobalta@users.noreply.github.com>
2024-03-13 14:25:38 +0000

    updates and remove redundant options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -12,50 +12,18 @@ local g = vim.g
 opt.laststatus = 3 -- global statusline
 opt.showmode = false
 
-opt.relativenumber = true
-
-opt.clipboard = "unnamedplus"
-opt.cursorline = true
-
--- Indenting
-opt.expandtab = true
-opt.shiftwidth = 2
-opt.smartindent = true
-opt.tabstop = 2
-opt.softtabstop = 2
-
-opt.fillchars = { eob = " " }
-opt.ignorecase = true
-opt.smartcase = true
-opt.mouse = "a"
-
--- Numbers
-opt.number = true
-opt.numberwidth = 2
-opt.ruler = false
+opt.relativenumber = false
 
 -- disable nvim intro
-opt.shortmess:append("sI")
+-- opt.shortmess:append("sI")
 
-opt.signcolumn = "yes"
-opt.splitbelow = true
-opt.splitright = true
-opt.termguicolors = true
-opt.timeoutlen = 400
-opt.undofile = true
+-- opt.swapfile = false
+-- opt.backup = false
 
-opt.swapfile = false
-opt.backup = false
-
-opt.scrolloff = 16 -- start scrolling when we are 8 lines away from margins
+opt.scrolloff = 8
 opt.colorcolumn = "100"
-opt.hlsearch = false
-opt.incsearch = true
 
-opt.wrap = true
-
--- interval for writing swap file to disk, also used by gitsigns
-opt.updatetime = 250
+-- opt.wrap = true
 
 -- go to previous/next line with h,l,left arrow and right arrow
 -- when cursor reaches end/beginning of line
