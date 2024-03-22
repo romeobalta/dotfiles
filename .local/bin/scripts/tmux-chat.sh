23a274d3766eb4bedaf79a6cd248fea72ef433e3
Romeo Balta <7095569+romeobalta@users.noreply.github.com>
2024-03-22 21:40:00 +0000

    tmux chat fix, tmux sessionizer folder fix and others

--- .local/bin/scripts/tmux-chat.sh
+++ .local/bin/scripts/tmux-chat.sh
@@ -6,9 +6,10 @@ fi
 
 read -p "Enter Query: " query
 
-if grep -qs "$selected" ~/.tmux-cht-languages; then
-	query=$(echo $query | tr ' ' '+')
-	tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+if grep -qs "$selected" ~/.tmux/.tmux-cht-languages; then
+	query=$(echo "$query" | tr ' ' '+')
+	# tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+	tmux neww bash -c "curl -s cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
 	tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
 fi
