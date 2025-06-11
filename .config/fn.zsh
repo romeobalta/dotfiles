3dbdcf316c4e06cac712bc6e68e371e1611cf6dd
Romeo Balta <7095569+romeobalta@users.noreply.github.com>
2025-06-11 14:31:35 +0100

    formatting

--- .config/fn.zsh
+++ .config/fn.zsh
@@ -1,26 +1,26 @@
 # Function to edit the current command buffer in nvim and execute on save
 edit-and-run() {
-  # Create a temporary file to store the command
-  local TMP_FILE=$(mktemp)
+    # Create a temporary file to store the command
+    local TMP_FILE=$(mktemp)
 
-  # Write the current command line buffer to the temp file
-  # $BUFFER is a special zsh variable holding the current command line
-  echo "$BUFFER" >"$TMP_FILE"
+    # Write the current command line buffer to the temp file
+    # $BUFFER is a special zsh variable holding the current command line
+    echo "$BUFFER" >"$TMP_FILE"
 
-  # Open the temp file in nvim. The script will pause here until you close nvim.
-  nvim "$TMP_FILE"
+    # Open the temp file in nvim. The script will pause here until you close nvim.
+    nvim "$TMP_FILE"
 
-  # After nvim closes, read the (potentially modified) command back
-  local NEW_CMD=$(<"$TMP_FILE")
+    # After nvim closes, read the (potentially modified) command back
+    local NEW_CMD=$(<"$TMP_FILE")
 
-  # Clean up the temporary file
-  rm -f "$TMP_FILE"
+    # Clean up the temporary file
+    rm -f "$TMP_FILE"
 
-  # Replace the current command buffer with the new command
-  BUFFER="$NEW_CMD"
+    # Replace the current command buffer with the new command
+    BUFFER="$NEW_CMD"
 
-  # Accept the command line to execute it
-  zle accept-line
+    # Accept the command line to execute it
+    zle accept-line
 }
 zle -N edit-and-run
 
