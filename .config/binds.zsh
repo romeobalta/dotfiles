67402f53e731b5af08f4867c87a74ed80e0ad759
Romeo Balta <7095569+romeobalta@users.noreply.github.com>
2025-06-11 14:26:27 +0100

    tmux-sessionizer widget

--- .config/binds.zsh
+++ .config/binds.zsh
@@ -1,4 +1,5 @@
-bindkey -s ^f "tmux-sessionizer\n"
+# bindkey -s ^f "tmux-sessionizer\n"
 bindkey -s ^g "lazygit\n"
-bindkey '^e' edit-and-run
 
+bindkey '^f' tmux-sessionizer
+bindkey '^e' edit-and-run
--- .config/fn.zsh
+++ .config/fn.zsh
@@ -22,5 +22,78 @@ edit-and-run() {
   # Accept the command line to execute it
   zle accept-line
 }
-
 zle -N edit-and-run
+
+tmux-sessionizer() {
+    # list of directories to search for
+    dirs=(
+        ~
+    )
+
+    # list of extra dirs
+    extra_dirs=(
+        # nested dirs
+        ~/personal/baremetal
+        ~/personal/tutorials
+    )
+
+    # for each extra dir, check if it exists and add it to the list
+    for dir in "${extra_dirs[@]}"; do
+        if [[ -d "$dir" ]]; then
+            dirs+=("$dir")
+        fi
+    done
+
+    # check if ~/projects/_archived exists and add it to the list
+    if [[ -d ~/projects/_archived ]]; then
+        dirs+=(~/projects/_archived)
+    fi
+
+    # check if ~/personal exists and add it to the list
+    if [[ -d ~/personal ]]; then
+        dirs+=(~/personal)
+    fi
+
+    # check if ~/work exists and add it to the list
+    if [[ -d ~/work ]]; then
+        dirs+=(~/work)
+    fi
+
+    # check if ~/soft exists and add it to the list
+    if [[ -d ~/soft ]]; then
+        dirs+=(~/soft)
+    fi
+
+    if [[ $# -eq 1 ]]; then
+        selected=$1
+    else
+        selected=$(find "${dirs[@]}" -mindepth 1 -maxdepth 1 -type d | fzf)
+    fi
+
+    if [[ -z $selected ]]; then
+        return 0
+        # exit 0
+    fi
+
+    selected_name=$(basename "$selected" | tr . _)
+    tmux_running=$(pgrep tmux)
+
+    if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+        tmux new-session -s "$selected_name" -c "$selected"
+        return 0
+        # exit 0
+    fi
+
+    if ! tmux has-session -t="$selected_name" 2>/dev/null; then
+        tmux new-session -ds "$selected_name" -c "$selected"
+    fi
+
+    if [[ -z $TMUX ]]; then
+        tmux attach-session -t "$selected_name"
+        return 0
+        # exit 0
+    fi
+
+    tmux switch-client -t "$selected_name"
+}
+zle -N tmux-sessionizer 
